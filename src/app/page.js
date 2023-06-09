95c25ca19cd7536af73ddfbd514bf6b6dfe60a58
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-10 00:13:22 +0700

    Add navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
+'use client'
+
+import Navbar from "@/components/Navbar";
+import { Container, VStack, Text, Divider } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <Container maxW='100vw' h='100vh' m='0' p='0' centerContent >
+      <Navbar />
+      <Divider h='2px' />
+      <VStack maxW={{ lg: '80vw' }} bg='green.300'>
+        <Container w='full'>Main body sfasdfsfgdgdgdfg</Container>
+        <Text w='100%'>Main body</Text>
+      </VStack>
+    </Container>
   )
 }
