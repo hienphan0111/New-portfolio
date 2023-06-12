0094e865653b27dbd9c724b46dfbc67e7f4ef02e
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-12 13:56:28 +0700

    Add about section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import { Container, VStack, Text, Divider } from '@chakra-ui/react';
@@ -10,9 +11,9 @@ export default function Home() {
     <main >
       <Navbar />
       <Divider border-bottom='2px' border-color='#469597' />
-      <VStack width={{ base: '100%', md: '80%' }} mx='auto' my='2'>
+      <VStack mx='auto' my='2'>
         <Hero />
-        
+        <About />
       </VStack>
     </main>
   )
